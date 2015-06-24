ff8212e0b692c78a8878493f8430143c934bdb8d
AL13F13 <dsauposafa@gmail.com>
2015-06-24 13:14:58 -0500

    dies
    se cambio el codigo

--- js/acciones.js
+++ js/acciones.js
@@ -1,11 +1,39 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	    $('#btnmenu').on('tap',function(){
+	    
+		$('#btnmenu').on('tap',function(){
 		navigator.vibrate(1000);
+		});
+		
+		$('#btnbiografia').on('tap',function(){
+		navigator.vibrate(1000);
+		});
+		
+		$('#btnalbunes').on('tap',function(){
+		navigator.vibrate(1000);
+		});
+		
+		$('#btnfotos').on('tap',function(){
+		navigator.vibrate(1000);
+		});
 		
 		
-	     }); 
+		$('#btndisco').on('tap',function(){
+		navigator.vibrate(1000);
+		});
+		
+		$('#btnintegrantes').on('tap',function(){
+		navigator.vibrate(1000);
+		});
+		
+		$('#btnvolver').on('tap',function(){
+		navigator.vibrate(1000);
+		});
+		
+		
+		
+		
 		
 		
 		
