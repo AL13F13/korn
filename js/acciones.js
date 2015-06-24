3555d6fc01d3f641d67786b9d20f96ca3702adaf
AL13F13 <dsauposafa@gmail.com>
2015-06-24 11:59:56 -0500

    seis
    se cambio el codigo de acciones

--- js/acciones.js
+++ js/acciones.js
@@ -6,32 +6,26 @@ document.addEventListener("deviceready",function(){
 	   
 		}); 
 		
-		$('#btnbiografia').on('tap',function() {
-		navigator.vibrate(1000);
+		$('#btnbiografia').on ('tap', function(){
+		navigator.notification.beep(1)	
+		});//Reinicio
 		
-		}); 
-		
-		$('#btnalbunes').on('tap',function() {
-		navigator.vibrate(1000);
-		
-		}); 
+		$('#btnalbunes').on ('tap', function(){
+		navigator.notification.beep(1)	
+		});//Reinicio
 		
 		
-		$('#btnfotos').on('tap',function() {
-		navigator.vibrate(1000);
+		$('#btnfotos').on ('tap', function(){
+		navigator.notification.beep(1)	
+		});//Reinicio
 		
-		}); 
+		$('#btndisco').on ('tap', function(){
+		navigator.notification.beep(1)	
+		});//Reinicio
 		
-		$('#btndisco').on('tap',function() {
-		navigator.vibrate(1000);
-		
-		}); 
-		
-		$('#btnintegrantes').on('tap',function() {
-		navigator.vibrate(1000);
-		
-		}); 
-		
+		$('#btnintegrantes').on ('tap', function(){
+		navigator.notification.beep(1)	
+		});//Reinicio
 		
 		
 	
