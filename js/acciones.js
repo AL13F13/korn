a56bfdada3e4c5ab3219eac3ffd0ac4afa73b8a2
AL13F13 <dsauposafa@gmail.com>
2015-06-24 11:35:44 -0500

    cuatro
    se cambio el codigo

--- js/acciones.js
+++ js/acciones.js
@@ -1,10 +1,40 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	$('#btnmenu').on ('tap',function() {
+//document.addEventListener("deviceready",function(){
+	$('#btnmenu').on('tap',function() {
 		navigator.vibrate(1000);
+	    alert('vibro');
+		}); 
 		
-}); 	
-}); 
+		$('#btnbiografia').on('tap',function() {
+		navigator.vibrate(1000);
+		alert('vibro');
+		}); 
+		
+		$('#btnalbunes').on('tap',function() {
+		navigator.vibrate(1000);
+		alert('vibro');
+		}); 
+		
+		
+		$('#btnfotos').on('tap',function() {
+		navigator.vibrate(1000);
+		alert('vibro');
+		}); 
+		
+		$('#btndisco').on('tap',function() {
+		navigator.vibrate(1000);
+		alert('vibro');
+		}); 
+		
+		$('#btnintegrantes').on('tap',function() {
+		navigator.vibrate(1000);
+		alert('vibro');
+		}); 
+		
+		
+		
+	
+//}); 
 });
 
