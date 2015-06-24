2aa0415b5cce70ac799dfbbab54fa34b80f8d464
AL13F13 <dsauposafa@gmail.com>
2015-06-24 13:01:55 -0500

    nueve
    se cambio el codigo

--- js/acciones.js
+++ js/acciones.js
@@ -3,30 +3,11 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	    $('#btnmenu').on('tap',function(){
 		navigator.vibrate(1000);
-		alert('vivro');
+		
 		
 	     }); 
 		
-		$('#btnbiografia').on('tap',function(){
-		navigator.vibrate(1000);
-		});//Reinicio
 		
-		$('#btnalbunes').on('tap',function(){
-		navigator.vibrate(1000);
-		});
-		
-		$('#btnfotos').on('tap',function(){
-		navigator.vibrate(1000);
-		});//Reinicio
-		
-		$('#btndisco').on('tap',function(){
-		navigator.vibrate(1000);
-		});//Reinicio
-		
-		$('#btnintegrantes').on('tap',function(){
-		navigator.vibrate(1000);
-		
-		});//Reinicio
 		
 		
 	
